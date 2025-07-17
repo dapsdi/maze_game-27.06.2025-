450b2784dd74385051706148cb902fc190b6d483
dapsdi <artemzg029@gmail.com>
2025-07-17 15:31:10 +0300

    біоми і їх характеристики

--- settings/constants.py
+++ settings/constants.py
@@ -15,6 +15,33 @@ tile_grid_size = 9  #розмір сітки тайлів у секторі
 #чому 9: у методі generate_tile_maze створюється локальний лабіринт розміром 9х9
 #кожен сектор на карті(в глобальній сітці екторів) має власний розмір(міні карту) із 9 тайлів
 
+#перелік доступних біомів
+BIOMES = {
+    "jungle": {
+        "background": "pictures/jungle_background.png",
+        "wall_color":  (66, 32, 128),
+    },
+
+    "swamp": {
+        "background": "pictures/swamp_background.png",
+        "wall_color":  (72, 66, 98),
+    },
+
+    "desert": {
+        "background": "pictures/desert_background.png",
+        "wall_color":  (69, 124, 168),
+    },
+
+    "mountain": {
+        "background": "pictures/mountain_background.png",
+        "wall_color":  (80, 0, 0),
+    },
+
+    "cave": {
+        "background": "pictures/cave_background.png",
+        "wall_color":  (59, 73, 89),
+    },
+}
 
 #мінімальний/максимальний зсув входу від краю сектора
 entry_offset_min = 2  
