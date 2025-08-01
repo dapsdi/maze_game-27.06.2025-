5c16fa5b0669cc1556de37c4adcfe113a8aae4d4
dapsdi <artemzg029@gmail.com>
2025-08-01 16:55:23 +0200

    зміни для рішення проблеми з відсутністю виходів у нових біомах

--- core/game.py
+++ core/game.py
@@ -49,20 +49,36 @@ class Game:
                   (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                     running = False
 
-            # отримуємо глобальні стіни поточного сектора
+            #отримуємо глобальні стіни поточного сектора
             sx, sy = self.player.get_sector()
             global_walls  = self.map_controller.get_sector_walls((sx, sy))
             tile_walls   = self.map_controller.get_tile_colliders((sx, sy))
-            all_walls = global_walls + tile_walls
+            walls = global_walls + tile_walls
 
-            # оновлюємо
-            self.player.update(dt, all_walls)
+            #оновлюємо 
+            self.player.update(dt, walls)
+            new_sector = None
+            if self.player.rect.right < 0 and sx > 0:
+                new_sector = (sx - 1, sy)
+            elif self.player.rect.left > const.sector_size and sx < const.map_width - 1:
+                new_sector = (sx + 1, sy)
+            elif self.player.rect.bottom < 0 and sy > 0:
+                new_sector = (sx, sy - 1)
+            elif self.player.rect.top > const.sector_size and sy < const.map_height - 1:
+                new_sector = (sx, sy + 1)
+
+            if new_sector is not None:
+                self.player.sector = new_sector
+                px, py = self.map_controller.get_safe_transition_point(new_sector)
+                self.player.rect.center = (px, py)
+                walls = self.map_controller.get_sector_walls(new_sector) + \
+                        self.map_controller.get_tile_colliders(new_sector)
+
             self.camera.update(self.player)
 
-            # малюємо
-            self.map_controller.draw_sector(self.screen, (sx, sy))
+            #рендер
+            self.map_controller.draw_sector(self.screen, (self.player.get_sector()))
             self.player.draw(self.screen)
-
             pygame.display.flip()
 
         pygame.quit()
